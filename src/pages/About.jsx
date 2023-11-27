f4a4de7dfa6956da4ce55b6ade20cfb70884e3ac
Lipika Mangla <131666154+LipikaManglaa@users.noreply.github.com>
2023-11-27 22:38:32 +1100

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../layout/Layout'
 import { Link } from 'react-router-dom'
 import aboutImg from './../assets/profile.jpg'
-import './../styles/About.css'
+import './styles/About.css'
 
 export default function About() {
    
